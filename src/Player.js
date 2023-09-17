2edf6dff636abeaaa4c4763031d8e48616817f8b
selasiii <52832049+selasiii@users.noreply.github.com>
2023-09-17 14:21:30 +0000

    add react player

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+function Player() {
+  return (
+    <div className='Player'>
+        <ReactPlayer/>
+    </div>
+  )
+}
+
+export default Player
